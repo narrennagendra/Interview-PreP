e76ee29155a28d69a5fd7120a42d8cfd681435e6
Nagendra K <77627407+narrennagendra@users.noreply.github.com>
2023-07-01 09:46:13 +0530

    fixed a bug

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const authController = require('../controllers/auth');
-const isAuth = require('../middleware/isAuth');
+const isAuth = require('../middleware/is-auth');
 const User = require('../models/user');
 
 const router = express.Router();

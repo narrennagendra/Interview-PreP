e7d2a7f82e7de1c38b9de8d4aba764e4a29cc162
Bhaskar <datlakarthik16@gmail.com>
2023-07-02 20:15:53 +0530

    styled pages and added routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 exports.getHome = (req, res, next) => {
-	res.render('home');
+	res.render('blog');
 };
+exports.getProblems = (req,res,next) => {
+	res.render('problems');
+}
+exports.getNewProblem = (req,res, next) => {
+	res.render('problemPost');
+}
+exports.getNewBlog = (req,res, next) => {
+	res.render('blogCreate');
+}

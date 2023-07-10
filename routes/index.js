0d685eb5f6ef3f57d1c486d202c46ce8e87553ac
nagendra <narrennagendra@gmail.com>
2023-07-10 19:45:17 +0530

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,10 @@ const homeController = require('../controllers/index');
 const isAuth = require('../middleware/is-auth');
 
 router.get('/', isAuth, homeController.getHome);
-router.get('/problems',isAuth, homeController.getProblems);
-router.get("/problemPost",isAuth,homeController.getNewProblem);
-router.get("/blogCreate",isAuth,homeController.getNewBlog);
-router.get("/viewProblem",isAuth,homeController.getProblem);
-router.get("/viewBlog",isAuth,homeController.getPost);
 router.get('/blog/:blogId', homeController.getBlog);
 router.get('/createBlog', isAuth, homeController.getCreateBlog);
 router.post('/createBlog', isAuth, homeController.postCreateBlog);
+router.get('/problems', isAuth, homeController.getProblems);
 router.get('/createProblem', isAuth, homeController.getCreateProblem);
 router.post('/createProblem', isAuth, homeController.postProblem);
 router.get('/problem/:problemId', isAuth, homeController.getProblem);
